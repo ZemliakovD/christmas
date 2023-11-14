775590fe8d7547b23632613035a165baf75019c7
Dmitri Zemliakov <112906798+ZemliakovD@users.noreply.github.com>
2023-11-14 04:32:36 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
-$('.vistavka-slider').slick({
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
+$("#start").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+    scrollTop: $(".start").offset().top // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
 
-  $('.shoy-slider').slick({
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
 
-  $('.prazdnik-slider').slick({
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
+$("#exhibition").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+    scrollTop: $(".exhibition").offset().top // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+
+$("#kids").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+    scrollTop: $(".kids").offset().top // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+
+$("#christmas").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+    scrollTop: $(".christmas").offset().top // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+
+$("#to-top").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+    scrollTop: $(".header").offset().top // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
